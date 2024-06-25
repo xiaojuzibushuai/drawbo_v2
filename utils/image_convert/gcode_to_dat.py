99ee9be668fbb57026c97891783e3af26a113450
xiaojuzi <1206085316@qq.com>
2024-06-25 10:00:57 +0800

    gcode文件合并生成

--- utils/image_convert/gcode_to_dat.py
+++ utils/image_convert/gcode_to_dat.py
@@ -70,8 +70,59 @@ def convert_gcode_to_dat(gcode_file_path, dat_file_path):
         dat_file.write(num1.to_bytes(2, 'little', signed=True))
 
 
+def merge_gcode_files(input_files, output_file):
+    with open(output_file, 'w') as output:
+        for idx, file_path in enumerate(input_files):
+            with open(file_path, 'r') as file:
+                file_content = file.readlines()
+
+                if idx == 0:
+                    # For the first file, write until the first "(end of print job)"
+                    end_of_print_job_index = next((i for i, line in enumerate(file_content) if '(end of print job)' in line), None)
+                    print(end_of_print_job_index)
+                    if end_of_print_job_index is not None:
+                        output.writelines(file_content[:end_of_print_job_index + 1])
+                    else:
+                        return None
+                elif idx == len(input_files) - 1:
+                    # For the last file, write everything until the end of the file
+                    polyline_index = next((i for i, line in enumerate(file_content) if '(Polyline consisting of 1 segments.)' in line), None)
+                    print(polyline_index)
+                    if polyline_index is not None:
+                        output.writelines(file_content[polyline_index:])
+                    else:
+                        return None
+
+                else:
+                    # For subsequent files, find "(Polyline consisting of 1 segments.)" and write until "(end of print job)"
+                    polyline_index = next((i for i, line in enumerate(file_content) if '(Polyline consisting of 1 segments.)' in line), None)
+                    if polyline_index is not None:
+                        end_of_print_job_index = next((i for i, line in enumerate(file_content) if '(end of print job)' in line and i > polyline_index), None)
+                        if end_of_print_job_index is not None:
+                            print(polyline_index,end_of_print_job_index)
+                            output.writelines(file_content[polyline_index:end_of_print_job_index + 1])
+                        else:
+                            return None
+                    else:
+                        return None
+
+    print(f'Merged {len(input_files)} gcode files successfully. Merged content saved to {output_file}')
+
 
 if __name__=='__main__':
-    gcode_dir = os.path.join(os.getcwd(),"utils","image_convert","gcode","hhh.gcode")
-    dat_dir = os.path.join(os.getcwd(),"utils","image_convert","dat","hhh.dat")
-    convert_gcode_to_dat("./gcode/bbb.gcode","./dat/bbb.dat")
+
+    input_files = [
+        '1.gcode',
+        '2.gcode',
+        '3.gcode'
+        # Add more files as needed
+    ]
+
+    output_file = 'merged_output.gcode'
+    merge_gcode_files(input_files, output_file)
+
+    # convert_gcode_to_dat(output_file,'out_merged.dat')
+
+    # gcode_dir = os.path.join(os.getcwd(),"utils","image_convert","gcode","hhh.gcode")
+    # dat_dir = os.path.join(os.getcwd(),"utils","image_convert","dat","hhh.dat")
+    # convert_gcode_to_dat("./gcode/bbb.gcode","./dat/bbb.dat")
